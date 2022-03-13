0bb94b3435bb0a35ac0993f769efa2a77fcd7ba1
Hamdyx <a.hamdy.nig@gmail.com>
2022-03-13 20:11:59 +0200

    Navbar style -created social and nav sections with initial theme

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,6 @@
 import { Link as NextLink } from 'next/link';
 import {
+	Container,
 	Link,
 	Center,
 	Box,
@@ -7,37 +8,70 @@ import {
 	Spacer,
 	Image,
 	Divider,
+	Icon,
+	Text,
 } from '@chakra-ui/react';
-import { Search2Icon } from '@chakra-ui/icons';
+import { Search2Icon, TimeIcon } from '@chakra-ui/icons';
+import { FaFacebookF, FaInstagram } from 'react-icons/fa';
 
 export default function Navbar() {
 	return (
 		<nav>
-			<HStack p={4} spacing={8}>
-				<Box bg="white" w="300px" p={4} color="white">
-					<Image src="/Pumptec-logo-copy.png" alt="Pump Tec Logo" />
+			<TopNav />
+
+			<Container maxW="container.xl" bg="white" centerContent pt={4} pb={4}>
+				<Box w="90%" color="black" layerStyle="navigation">
+					<HStack spacing={8}>
+						<Box w="300px" className="logo-box">
+							<Image src="/Pumptec-logo-copy.png" alt="Pump Tec Logo" />
+						</Box>
+						<Spacer />
+						<NavLink route="home" />
+
+						<NavLink route="about" />
+						<NavLink route="services" />
+						<NavLink route="products" />
+						<NavLink route="contact" />
+						<Center height="30px">
+							<Divider orientation="vertical" bg="gray" />
+						</Center>
+						<Search2Icon />
+					</HStack>
 				</Box>
-				<Spacer />
-				<Link as={NextLink} to="/home">
-					home
-				</Link>
-				<Link as={NextLink} to="/about">
-					about
-				</Link>
-				<Link as={NextLink} to="/services">
-					services
-				</Link>
-				<Link as={NextLink} to="/products">
-					products
-				</Link>
-				<Link as={NextLink} to="/contact">
-					contact
-				</Link>
-				<Center height="30px">
-					<Divider orientation="vertical" />
-				</Center>
-				<Search2Icon />
-			</HStack>
+			</Container>
 		</nav>
+	);
+}
+
+function TopNav() {
+	return (
+		<Container maxW="container.xl" bg="blue.900" pt={2} pb={2} centerContent>
+			<Box w="90%" color="white">
+				<HStack spacing={2}>
+					<TimeIcon />
+					<Text fontSize="xs">We'are Open: Sunday - Thursday 9:00 - 18:00</Text>
+					<Spacer />
+					<Link as={NextLink} className="socialLink" to="/facebook">
+						<Icon as={FaFacebookF} />
+					</Link>
+					<Link as={NextLink} className="socialLink" to="/instagram">
+						<Icon as={FaInstagram} />
+					</Link>
+				</HStack>
+			</Box>
+		</Container>
+	);
+}
+
+function NavLink({ route }) {
+	let isActive = route === 'home' ? true : false;
+	return (
+		<Link
+			as={NextLink}
+			className={`nav-item ${isActive ? 'active' : ''}`}
+			to={`/${route === 'home' ? '' : route}`}
+		>
+			{route}
+		</Link>
 	);
 }
