c21d3da3a8f20cbc16502bf63c13a709732b6fe9
Hamdyx <a.hamdy.nig@gmail.com>
2022-03-20 18:36:18 +0200

    footer contact box init

--- components/footer.js
+++ components/footer.js
@@ -4,6 +4,7 @@ import {
 	Link as ChakraLink,
 	Flex,
 	Box,
+	Center,
 	VStack,
 	HStack,
 	Image,
@@ -20,18 +21,70 @@ import {
 } from '@chakra-ui/react';
 import { Search2Icon } from '@chakra-ui/icons';
 import { FaFacebookF, FaInstagram } from 'react-icons/fa';
+import { FiMapPin } from 'react-icons/fi';
+import { BiPhoneCall } from 'react-icons/bi';
+import { BsEnvelopeOpen } from 'react-icons/bs';
 
 export default function Footer() {
 	return (
 		<footer>
 			<Container
 				maxW="container.2xl"
-				bg="blue.700"
+				bg="blue.900"
 				centerContent
 				pt={4}
 				pb={4}
 				mt={20}
 			>
+				<Flex color="white" mb={8}>
+					<Center w="350px" h="100px" bg="gray.800" px={5}>
+						<HStack w="100%" spacing={8}>
+							<Box w="50px" className="logo-box">
+								<Text>
+									<Icon as={FiMapPin} w={10} h={10} />
+								</Text>
+							</Box>
+							<VStack align="left">
+								<Heading as="h3" size="md">
+									New Maadi, Cairo
+								</Heading>
+								<Text>
+									5 El-lathy St. From El-Naser Street, New Maadi Cairo, Egypt
+								</Text>
+							</VStack>
+						</HStack>
+					</Center>
+					<Center w="350px" h="100px" bg="red.700" px={5}>
+						<HStack w="100%" spacing={8}>
+							<Box w="50px" className="logo-box">
+								<Text>
+									<Icon as={BiPhoneCall} w={10} h={10} />
+								</Text>
+							</Box>
+							<VStack align="left">
+								<Heading as="h3" size="md">
+									+20 100 052 9308
+								</Heading>
+								<Text>Give us a call</Text>
+							</VStack>
+						</HStack>
+					</Center>
+					<Center w="350px" h="100px" bg="red.500" px={5}>
+						<HStack w="100%" spacing={8}>
+							<Box w="50px" className="logo-box">
+								<Text>
+									<Icon as={BsEnvelopeOpen} w={10} h={10} />
+								</Text>
+							</Box>
+							<VStack align="left">
+								<Heading as="h3" size="md">
+									info@pump-tec.net
+								</Heading>
+								<Text>Drop us a linel</Text>
+							</VStack>
+						</HStack>
+					</Center>
+				</Flex>
 				<Flex w="90%" layerStyle="footer">
 					<SimpleGrid columns={4} spacing={10}>
 						<VStack spacing={8}>
