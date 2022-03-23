ddbb644f592448193cf25af88362f598151b3583
Hamdyx <a.hamdy.nig@gmail.com>
2022-03-23 18:34:32 +0200

    404 page init

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+export default function Custom404() {
+	return (
+		<div id="container-404">
+			<div className="content">
+				<h2>404</h2>
+				<h4>Page not found</h4>
+				<p>
+					The page you were looking for doesn't exist. You may have mistyped the
+					address or the page may have moved.
+				</p>
+				<a href="/">Back To Home</a>
+			</div>
+		</div>
+	);
+}
